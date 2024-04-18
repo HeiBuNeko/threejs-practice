696e86b40ca90838d5b5fc941dda514ef0f53eff
猫宫黑布 <88360720+HeiBuNeko@users.noreply.github.com>
2024-04-18 18:25:56 +0800

    物体分组管理

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@
 <script setup lang="ts">
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/examples/jsm/libs/stats.module.js'
 import * as dat from 'dat.gui'
 
 // 场景初始化
@@ -20,9 +21,40 @@ document.body.appendChild(renderer.domElement)
 
 // 立方体渲染
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+// 1. 定义颜色数组（x 正负，y 正负，z 正负）
+const colorArr = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff]
+// 2. 遍每个颜色字符串映射成材质对象
+const cubeMaterialArr = colorArr.map((color) => new THREE.MeshBasicMaterial({ color }))
+// 3. 将材质对象数组传入 Mesh 构造函数
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterialArr)
 scene.add(cube)
+
+// 组
+const group = new THREE.Group()
+const cubeInfo = [
+  { color: 0x00ffff, w: 1, h: 1, d: 1, x: 2, y: 0, z: 0 },
+  { color: 0xff00ff, w: 1, h: 1, d: 1, x: 0, y: 0, z: 2 }
+]
+cubeInfo.forEach(({ color, w, h, d, x, y, z }) => {
+  const cubeGeometry = new THREE.BoxGeometry(w, h, d)
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color })
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+  cube.position.set(x, y, z)
+  group.add(cube)
+})
+scene.add(group)
+
+// 移除立方体
+window.addEventListener('dblclick', () => {
+  // 1. 调用相关废弃函数
+  group.children.forEach((child: any) => {
+    child.geometry.dispose()
+    child.material.dispose()
+    group.remove(child)
+  })
+  // 2. 从场景中移除立方体
+  scene.remove(group)
+})
 
 // 线条渲染
 const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff })
@@ -56,19 +88,44 @@ const gui = new dat.GUI()
 gui.add(document, 'title')
 gui.add(cube, 'visible')
 gui.add(controls, 'reset')
-const colorObj = { color: `#${cube.material.color.getHexString()}` }
+// 控制立方体颜色
+const colorObj = { color: `#${cube.material[0].color.getHexString()}` }
 gui.addColor(colorObj, 'color').onChange((e) => {
-  cube.material.color.set(e)
+  cube.material[0].color.set(e)
+})
+// 创建分组-影响立方体位置
+const folder = gui.addFolder('位移')
+// 参数：最小值范围，最大值范围，步长
+folder.add(cube.position, 'x', 0, 5, 0.1)
+folder.add(cube.position, 'y', 0, 5, 0.1)
+folder.add(cube.position, 'z', 0, 5, 0.1)
+// 下拉菜单-第三个参数为对象时->下拉菜单
+// 对象中属性名->下拉菜单选项名
+// 初始值匹配后会影响下拉菜单默认选中哪一项
+gui.add({ type: '1' }, 'type', { 选项1: '1', 选项2: '2' }).onChange((e) => {
+  switch (e) {
+    case '1':
+      cube.position.set(0, 0, 0)
+      break
+    case '2':
+      cube.position.set(1, 1, 1)
+      break
+  }
 })
 
-// 动画
-const animate = () => {
+// 性能监控
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+// 循环渲染
+const renderLoop = () => {
   controls.update()
+  stats.update()
   renderer.render(scene, camera)
-  requestAnimationFrame(animate)
+  requestAnimationFrame(renderLoop)
 }
 
-animate()
+renderLoop()
 </script>
 
 <style scoped lang="scss">
