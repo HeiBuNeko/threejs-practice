aa90499dab9bb80584528f0c5df4600aacad29f1
猫宫黑布 <88360720+HeiBuNeko@users.noreply.github.com>
2024-04-19 18:11:00 +0800

    Threejs基础-点线面材质

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,13 +16,13 @@
           <el-col :span="12">
             <el-card class="card-container" @click="toPractice">
               <template #header> Threejs Practice</template>
-              <el-image fit="fill" src="/ThreejsPrctice.jpg" />
+              <el-image fit="fill" :src="ThreejsPrctice" />
             </el-card>
           </el-col>
           <el-col :span="12">
             <el-card class="card-container">
               <template #header>Coming Soon</template>
-              <el-image fit="fill" src="/ThreejsPrctice.jpg" /> </el-card
+              <el-image fit="fill" :src="ThreejsPrctice" /> </el-card
           ></el-col>
         </el-row>
       </el-main>
@@ -35,6 +35,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { Sunny, Moon } from '@element-plus/icons-vue'
+import ThreejsPrctice from '@/assets/ThreejsPrctice.jpg'
 
 // 暗黑模式
 const isDark = ref(true)
--- src/views/ThreejsPractice/ThreejsPracticeIndex.vue
+++ src/views/ThreejsPractice/ThreejsPracticeIndex.vue
@@ -19,30 +19,82 @@ const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-// 立方体渲染
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+// 立方缓冲几何体
+const boxGeo = new THREE.BoxGeometry(1, 1, 1)
 // 1. 定义颜色数组（x 正负，y 正负，z 正负）
 const colorArr = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff]
 // 2. 遍每个颜色字符串映射成材质对象
-const cubeMaterialArr = colorArr.map((color) => new THREE.MeshBasicMaterial({ color }))
+const boxMatArr = colorArr.map((color) => new THREE.MeshBasicMaterial({ color }))
 // 3. 将材质对象数组传入 Mesh 构造函数
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterialArr)
-scene.add(cube)
+const box = new THREE.Mesh(boxGeo, boxMatArr)
+scene.add(box)
 
 // 组
 const group = new THREE.Group()
-const cubeInfo = [
+const boxInfo = [
   { color: 0x00ffff, w: 1, h: 1, d: 1, x: 2, y: 0, z: 0 },
-  { color: 0xff00ff, w: 1, h: 1, d: 1, x: 0, y: 0, z: 2 }
+  { color: 0x0000ff, w: 1, h: 1, d: 1, x: 4, y: 0, z: 0 }
 ]
-cubeInfo.forEach(({ color, w, h, d, x, y, z }) => {
-  const cubeGeometry = new THREE.BoxGeometry(w, h, d)
-  const cubeMaterial = new THREE.MeshBasicMaterial({ color })
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-  cube.position.set(x, y, z)
-  group.add(cube)
+boxInfo.forEach(({ color, w, h, d, x, y, z }) => {
+  const boxGeo = new THREE.BoxGeometry(w, h, d)
+  const boxMat = new THREE.MeshBasicMaterial({ color })
+  const box = new THREE.Mesh(boxGeo, boxMat)
+  box.position.set(x, y, z)
+  group.add(box)
 })
 scene.add(group)
+
+// 球缓冲几何体
+const sphereGeo = new THREE.SphereGeometry(0.5, 32, 32)
+const sphereMat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
+const sphere = new THREE.Mesh(sphereGeo, sphereMat)
+sphere.position.set(6, 0, 0)
+scene.add(sphere)
+
+// 圆形缓冲几何体
+const circleGeo = new THREE.CircleGeometry(0.5, 128)
+const circleMat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
+const circle = new THREE.Mesh(circleGeo, circleMat)
+circle.position.set(4, 2, 0)
+scene.add(circle)
+
+// 平面缓冲几何体
+const planeGeo = new THREE.PlaneGeometry(1, 1)
+const planeMat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
+const plane = new THREE.Mesh(planeGeo, planeMat)
+plane.position.set(2, 2, 0)
+scene.add(plane)
+
+// 点
+const pointMat = new THREE.PointsMaterial({ color: 0x00ffff, size: 0.01 })
+const pointSphere = new THREE.Points(sphereGeo, pointMat)
+pointSphere.position.set(6, 2, 0)
+scene.add(pointSphere)
+
+// 线
+const lineMat = new THREE.LineBasicMaterial({ color: 0x0000ff })
+const linePoints = []
+linePoints.push(new THREE.Vector3(-5, 0, 0))
+linePoints.push(new THREE.Vector3(0, 5, 0))
+linePoints.push(new THREE.Vector3(5, 0, 0))
+const lineGeo = new THREE.BufferGeometry().setFromPoints(linePoints)
+// Line 线 LineLoop 环线 LineSegments 线段
+const line = new THREE.Line(lineGeo, lineMat)
+scene.add(line)
+
+// 星空
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+  const x = THREE.MathUtils.randFloatSpread(2000)
+  const y = THREE.MathUtils.randFloatSpread(2000)
+  const z = THREE.MathUtils.randFloatSpread(2000)
+  starVertices.push(x, y, z)
+}
+const starGeo = new THREE.BufferGeometry()
+starGeo.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
+const starMat = new THREE.PointsMaterial({ color: 0x888888 })
+const points = new THREE.Points(starGeo, starMat)
+scene.add(points)
 
 // 移除立方体
 window.addEventListener('dblclick', () => {
@@ -55,16 +107,6 @@ window.addEventListener('dblclick', () => {
   // 2. 从场景中移除立方体
   scene.remove(group)
 })
-
-// 线条渲染
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff })
-const linePoints = []
-linePoints.push(new THREE.Vector3(-5, 0, 0))
-linePoints.push(new THREE.Vector3(0, 5, 0))
-linePoints.push(new THREE.Vector3(5, 0, 0))
-const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints)
-const line = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(line)
 
 // 轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -86,29 +128,29 @@ window.addEventListener('resize', () => {
 // 图形用户界面
 const gui = new dat.GUI()
 gui.add(document, 'title')
-gui.add(cube, 'visible')
+gui.add(box, 'visible')
 gui.add(controls, 'reset')
 // 控制立方体颜色
-const colorObj = { color: `#${cube.material[0].color.getHexString()}` }
+const colorObj = { color: `#${box.material[0].color.getHexString()}` }
 gui.addColor(colorObj, 'color').onChange((e) => {
-  cube.material[0].color.set(e)
+  box.material[0].color.set(e)
 })
 // 创建分组-影响立方体位置
 const folder = gui.addFolder('位移')
 // 参数：最小值范围，最大值范围，步长
-folder.add(cube.position, 'x', 0, 5, 0.1)
-folder.add(cube.position, 'y', 0, 5, 0.1)
-folder.add(cube.position, 'z', 0, 5, 0.1)
+folder.add(box.position, 'x', 0, 5, 0.1)
+folder.add(box.position, 'y', 0, 5, 0.1)
+folder.add(box.position, 'z', 0, 5, 0.1)
 // 下拉菜单-第三个参数为对象时->下拉菜单
 // 对象中属性名->下拉菜单选项名
 // 初始值匹配后会影响下拉菜单默认选中哪一项
 gui.add({ type: '1' }, 'type', { 选项1: '1', 选项2: '2' }).onChange((e) => {
   switch (e) {
     case '1':
-      cube.position.set(0, 0, 0)
+      box.position.set(0, 0, 0)
       break
     case '2':
-      cube.position.set(1, 1, 1)
+      box.position.set(1, 1, 1)
       break
   }
 })
